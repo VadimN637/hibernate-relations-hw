c9215b9993a21615f1600fd724cc5378fa51d459
VadimN637 <turnedofff@gmail.com>
2026-05-08 21:26:12 +0300

    6

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -17,7 +17,7 @@ public class Movie implements Cloneable {
     private Long id;
     private String title;
 
-    @OneToMany(mappedBy = "movie")
+    @OneToMany
     private List<Actor> actors;
 
     public Movie() {
